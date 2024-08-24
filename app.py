69fe0225561221c08a142bcba6dba4949e37b76c
Owen Nicholas Yap <oyap2552@uni.sydney.edu.au>
2024-08-24 13:42:02 +1000

    index.html

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with app.app_context():
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
