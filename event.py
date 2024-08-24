34bbb9805f7da7d8cd2c1452e870d38452b66ea1
cyrus60211 <cyrus60211@gmail.com>
2024-08-24 21:57:49 +1000

    push

--- event.py
+++ event.py
@@ -1,40 +1,24 @@
-from flask import Flask, request, render_template
 import sqlite3
-import os
+from jinja2 import Environment, FileSystemLoader
 
-app = Flask(__name__)
+db_path = 'instance/users.db'
+html_template_path = 'templates/event_template.html'
+output_html_path = 'templates/event.html'
 
-@app.route('/events', methods=['GET'])
-def events():
-    # Get the date from the query parameter
-    filter_date = request.args.get('date')
-    
-    # Connect to the database
-    db_path = os.path.join('instance', 'users.db')
-    
-    if not os.path.exists(db_path):
-        return "Database file not found.", 500
-    
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    
-    if filter_date:
-        # Filter events based on the input date
-        query = '''
-        SELECT * FROM event
-        WHERE date = ?
-        '''
-        cursor.execute(query, (filter_date,))
-    else:
-        # Retrieve all events if no date is provided
-        query = 'SELECT * FROM event'
-        cursor.execute(query)
-    
-    events = cursor.fetchall()
-    conn.close()
-    
-    # Render the template with the filtered events
-    return render_template('event.html', events=events)
+conn = sqlite3.connect(db_path)
+cursor = conn.cursor()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+cursor.execute('SELECT * FROM event')
+events = cursor.fetchall()
+
+conn.close()
+
+env = Environment(loader=FileSystemLoader('templates'))
+template = env.get_template('event_template.html')
+
+html_content = template.render(events=events)
+
+with open(output_html_path, 'w') as file:
+    file.write(html_content)
+
+print(f'HTML file generated: {output_html_path}')
