adee66e147c92468ab98d4dcd88aa79dc7ff82cc
Owen Nicholas Yap <oyap2552@uni.sydney.edu.au>
2024-08-25 02:00:43 +1000

    make 2 tables at once

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from models import db, User
 import sqlite3
 import os
 
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
 
 import sqlite3
 from jinja2 import Environment, FileSystemLoader
@@ -282,6 +285,83 @@ with open(output_html_path, 'w') as file:
 print(f'HTML file generated: {output_html_path}')
 '''
 
+def scrape_and_store_events():
+    conn = sqlite3.connect(r'instance/users.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS event (
+        eventid INTEGER PRIMARY KEY,
+        title VARCHAR(100),
+        date DATE,
+        start TIME,
+        end TIME,
+        dow VARCHAR(20)
+    );
+    ''')
+
+    url = 'https://usu.edu.au/events/'
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    events = soup.find_all('div', class_='EventCard-module--EventCard--3dc8c')
+
+    eventsdata = []
+    id = 0
+    for card in events:
+        date_span = card.find('span', class_='EventCard-module--date--d8888')
+        date = date_span.get_text(strip=True)
+        test1 = date.split("-")
+        if len(test1) == 2:
+            test2 = test1[1].split("to")
+        elif len(test1) == 1:
+            test2 = test1[0].split("to")
+        d0 = test2[0].split()
+        d = d0[0] + " " + d0[1]
+        start = d0[2] + " " + d0[3]
+        end = test2[1].strip()
+        y = 2024
+        date_str = f"{d} {y}"
+        date_format_in = "%d %b %Y"
+        date_obj = datetime.strptime(date_str, date_format_in)
+        fdate = date_obj.strftime("%Y-%m-%d")
+        time_format_in = "%I:%M %p"
+        time_format_out = "%H:%M"
+
+        def convert_time(time_str):
+            time_obj = datetime.strptime(time_str, time_format_in)
+            return time_obj.strftime(time_format_out)
+
+        fstart = convert_time(start)
+        fend = convert_time(end)
+
+        date_obj = datetime.strptime(fdate, "%Y-%m-%d")
+
+        dow = date_obj.strftime("%A")
+
+        title_h3 = card.find('h3', class_='EventCard-module--name--c1353')
+        title = title_h3.get_text(strip=True)
+
+        allowed_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+        filtered_characters = []
+
+        for char in title:
+            if char in allowed_characters:
+                filtered_characters.append(char)
+
+        result = ''.join(filtered_characters)
+
+        id += 1
+        sql = '''INSERT OR IGNORE INTO event (eventid, title, date, start, end, dow) 
+                VALUES (?, ?, ?, ?, ?, ?);
+                '''
+        cursor.execute(sql, (id, result, fdate, fstart, fend, dow))
+
+    conn.commit()
+    conn.close()
+
 
 
 
@@ -290,4 +370,6 @@ print(f'HTML file generated: {output_html_path}')
 
 
 if __name__ == '__main__':
-   app.run(host='127.0.0.1', port=5001, debug=True)
+    with app.app_context():
+        scrape_and_store_events()
+    app.run(host='127.0.0.1', port=5001, debug=True)
